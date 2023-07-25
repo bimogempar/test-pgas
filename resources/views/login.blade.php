5b396e0e7fa5a53e453221a057f9da7a6ddaf39f
bimogempar <bimogempar@gmail.com>
2023-07-25 17:40:21 +0700

    update: adding UI for login and register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,32 +1,25 @@
-<!DOCTYPE html>
-<html>
+@extends('layout')
 
-<head>
-    <title>Login</title>
-</head>
-
-<body>
-    <a href="{{ route('register') }}">Register page</a>
-    <form method="POST" action="{{ route('doLogin') }}">
-        @csrf
-
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
-        </div>
-
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
+@section('content')
+    <div class="flex justify-center items-center min-h-screen">
+        <form method="POST" action="{{ route('doLogin') }}" class="flex flex-col gap-4 w-1/2 bg-gray-100 p-4 rounded-lg">
+            @csrf
+            <div class="text-xl font-medium text-center">Login</div>
+            <div class="flex flex-col gap-2">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="bg-white border-2 p-2 rounded-lg" required>
+            </div>
+            <div class="flex flex-col gap-2">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="bg-white border-2 p-2 rounded-lg" required>
+            </div>
+            <a href="{{ route('register') }}" class="text-blue-500 underline underline-offset-auto">Register ?</a>
+            <div class="flex justify-end">
+                <button type="submit" class="p-2 rounded-lg bg-blue-500 text-white w-24">Login</button>
+            </div>
+        </form>
+    </div>
     @error('errorLogin')
-        <div style="color: red;">{{ $message }}</div>
+        <div class="text-red-500">{{ $message }}</div>
     @enderror
-</body>
-
-</html>
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>Register</title>
-</head>
+@extends('layout')
 
-<body>
-    <a href="{{ route('login') }}">Login page</a>
-    <form method="POST" action="{{ route('doRegister') }}">
-        @csrf
+@section('content')
+    <div class="flex justify-center items-center min-h-screen">
+        <form method="POST" action="{{ route('doRegister') }}" class="flex flex-col gap-4 w-1/2 bg-gray-100 p-4 rounded-lg">
+            @csrf
+            <div class="text-xl font-medium text-center">Register</div>
+            <div class="flex flex-col gap-2">
+                <label for="name">Nama</label>
+                <input type="text" name="name" id="name" class="bg-white border-2 p-2 rounded-lg" required>
+            </div>
 
-        <div>
-            <label for="name">Nama</label>
-            <input type="text" name="name" id="name" required>
-        </div>
+            <div class="flex flex-col gap-2">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="bg-white border-2 p-2 rounded-lg" required>
+            </div>
 
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
-        </div>
+            <div class="flex flex-col gap-2">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="bg-white border-2 p-2 rounded-lg" required>
+            </div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-
-        <div>
-            <label for="role">Role</label>
-            <select name="role" id="role" required>
-                <option value="admin" selected>Admin</option>
-                <option value="user">User</option>
-            </select>
-        </div>
-
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-</body>
-
-</html>
+            <div class="flex flex-col gap-2">
+                <label for="role">Role</label>
+                <select name="role" id="role" class="bg-white border-2 p-2 rounded-lg" required>
+                    <option value="admin" selected>Admin</option>
+                    <option value="user">User</option>
+                </select>
+            </div>
+            <a href="{{ route('login') }}" class="text-blue-500 underline underline-offset-auto">Login ?</a>
+            <div class="flex justify-end">
+                <button type="submit" class="p-2 rounded-lg bg-blue-500 text-white w-24">Register</button>
+            </div>
+        </form>
+    </div>
+    @error('errorLogin')
+        <div class="text-red-500">{{ $message }}</div>
+    @enderror
+@endsection
